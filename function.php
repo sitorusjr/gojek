40e5c1f8254dc84cfebbed9f4213ad5a42f6c4ef
sitorusjr <62225983+sitorusjr@users.noreply.github.com>
2020-03-17 04:01:55 +0700

    update function.php

--- function.php
+++ function.php
@@ -11,7 +11,7 @@ $header[] = "Content-Type: application/json; charset=UTF-8";
 $header[] = "X-AppVersion: 3.46.1"; // ubah sesuai clone lu
 $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = "Connection: keep-alive";
-$header[] = "X-User-Locale: en_ID";
+$header[] = "X-User-Locale: id_ID";
 $header[] = "X-Location: -7.2474107,112.7492658";
 $header[] = "X-Location-Accuracy: 3.0";
 if ($pin):
